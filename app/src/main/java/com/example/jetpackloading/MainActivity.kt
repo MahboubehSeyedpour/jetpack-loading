83bc066b5023fcee3e215483180c1db76066e4f0
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2023-07-31 20:53:20 +0330

    feat: add grid_pulsating_dot animation

--- app/src/main/java/com/example/jetpackloading/MainActivity.kt
+++ app/src/main/java/com/example/jetpackloading/MainActivity.kt
@@ -3,15 +3,26 @@ package com.example.jetpackloading
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackloading.ui.theme.JetpackLoadingTheme
+import com.example.jetpackloading.ui.theme.background
+import com.example.jetpackloading.ui.theme.loading_component.GridPulsatingDot
 import com.example.jetpackloading.ui.theme.loading_component.PulsatingDot
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +32,8 @@ class MainActivity : ComponentActivity() {
             JetpackLoadingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
@@ -33,7 +45,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    PulsatingDot(color = Color.Black)
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(background)
+            .padding(40.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        PulsatingDot(color = Color.White)
+
+        Spacer(modifier = Modifier.width(50.dp))
+
+        GridPulsatingDot(color = Color.White, rowCount = 3, colCount = 3)
+    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/jetpackloading/ui/theme/loading_component/GridPulsatingDot.kt
@@ -0,0 +1,85 @@
+package com.example.jetpackloading.ui.theme.loading_component
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GridPulsatingDot(
+    color: Color,
+    dotSize: Dp = 10.dp,
+    pulseDelay: Int = 300,
+    rowCount: Int = 3,
+    colCount: Int = 3
+) {
+    val infiniteTransition = rememberInfiniteTransition()
+
+    @Composable
+    fun Dot(
+        scale: Float
+    ) = Spacer(
+        Modifier
+            .size(dotSize)
+            .scale(scale)
+            .background(
+                color = color,
+                shape = CircleShape
+            )
+    )
+
+    @Composable
+    fun PulsatingDotWithDelay(delay: Int) = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = pulseDelay * 4
+                0f at delay with LinearEasing
+                1f at delay + pulseDelay with LinearEasing
+                0f at delay + pulseDelay * 2
+            }
+        )
+    )
+
+    val dots: MutableList<State<Float>> = mutableListOf()
+
+    for (rowIndex in 0 until rowCount) {
+        for (colIndex in 0 until colCount) {
+            dots.add(PulsatingDotWithDelay(delay = (pulseDelay * (rowIndex * colCount + colIndex) * 0.3).toInt()))
+        }
+    }
+
+    Column {
+        val spaceSize = 2.dp
+        for (rowIndex in 0 until rowCount) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                for (colIndex in 0 until colCount) {
+                    val flatListIndex = rowIndex * colCount + colIndex
+                    Dot(dots[flatListIndex].value)
+                    Spacer(Modifier.width(spaceSize))
+                }
+            }
+        }
+    }
+}
