914de9cc9e652f85e6fd5f39c45a6c0fc8221289
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2023-09-14 17:01:30 +0330

    refactor: refactor PacmanIndicator

--- app/src/main/java/com/example/jetpackloading/ui/theme/loading_component/PacmanIndicator.kt
+++ app/src/main/java/com/example/jetpackloading/ui/theme/loading_component/PacmanIndicator.kt
@@ -7,85 +7,62 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.jetpackloading.ANIMATION_DEFAULT_COLOR
 
 @Composable
 fun PacmanIndicator(
     color: Color = ANIMATION_DEFAULT_COLOR,
+    ballDiameter: Float = 40f,
+    canvasSize: Dp = 40.dp,
+    animationDuration: Int = 500
 ) {
 
+    val lipStart = 0f
+    val lipEnd = 45f
+
     val positionAnimation by rememberInfiniteTransition().animateFloat(
-        initialValue = 30f,
-        targetValue = -12f,
+        initialValue = ballDiameter,
+        targetValue = -ballDiameter,
         animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 900, easing = FastOutSlowInEasing),
+            animation = tween(durationMillis = animationDuration, easing = FastOutSlowInEasing),
             repeatMode = RepeatMode.Restart
         )
     )
 
-    val upperLipAnimation by rememberInfiniteTransition().animateFloat(
-        initialValue = 360F,
-        targetValue = 290F,
+    val lipAnimation by rememberInfiniteTransition().animateFloat(
+        initialValue = lipStart,
+        targetValue = lipEnd,
         animationSpec = infiniteRepeatable(
-            animation = tween(600, easing = FastOutSlowInEasing),
-            repeatMode = RepeatMode.Restart
+            animation = tween(animationDuration / 2, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
         )
     )
 
-    val lowerLipAnimation by rememberInfiniteTransition().animateFloat(
-        initialValue = 0F,
-        targetValue = 40F,
-        animationSpec = infiniteRepeatable(
-            animation = tween(600, easing = FastOutSlowInEasing),
-            repeatMode = RepeatMode.Restart
+    Canvas(modifier = Modifier.size(canvasSize)) {
+        drawArc(
+            color = color,
+            startAngle = lipAnimation,
+            sweepAngle = 360 - lipAnimation.times(2),
+            topLeft = Offset(0f, 0f),
+            size = Size(size.width, size.height),
+            useCenter = true
         )
-    )
-
-
-    Box(modifier = Modifier.size(50.dp), contentAlignment = Alignment.Center) {
-
-        Canvas(
-            modifier = Modifier
-                .size(40.dp)
-                .align(Alignment.Center)
-        ) {
-            drawArc(color, upperLipAnimation, lowerLipAnimation)
-        }
-
-        Box(
-            modifier = Modifier
-                .offset(positionAnimation.dp)
-                .align(Alignment.Center)
-                .size(12.dp)
-                .clip(CircleShape)
-                .background(color)
-
-        ) {}
+        drawCircle(
+            color = color,
+            radius = ballDiameter / 2,
+            center = Offset(
+                x = size.width + positionAnimation,
+                y = size.height / 2
+            )
+        )
     }
-}
-
-private fun DrawScope.drawArc(color: Color, upperAnimation: Float, lowerAnimation: Float) {
-    drawArc(
-        color = color,
-        startAngle = lowerAnimation,
-        sweepAngle = upperAnimation,
-        topLeft = Offset(0f, 0f),
-        size = Size(size.width, size.height),
-        useCenter = true
-    )
 }
