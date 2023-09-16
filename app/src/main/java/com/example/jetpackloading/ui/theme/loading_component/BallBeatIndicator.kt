674154952d6c1d562f87155dfad55bc343ca6d1a
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2023-09-16 22:09:36 +0330

    refactor: refactor BallBeatIndicator

--- app/src/main/java/com/example/jetpackloading/ui/theme/loading_component/BallBeatIndicator.kt
+++ app/src/main/java/com/example/jetpackloading/ui/theme/loading_component/BallBeatIndicator.kt
@@ -5,8 +5,6 @@ import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,30 +12,36 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.example.jetpackloading.ANIMATION_DEFAULT_COLOR
 import kotlinx.coroutines.delay
 
 @Composable
 fun BallBeatIndicator(
     color: Color = ANIMATION_DEFAULT_COLOR,
-    ballCount: Int = 3
+    ballCount: Int = 3,
+    ballDiameter: Float = 40f,
+    spaceBetweenBalls: Float = 20f,
+    animationDuration: Int = 350,
+    animationDelay: Long = 200,
+    minAlpha: Float = 0.7f,
+    maxAlpha: Float = 1f
 ) {
 
-    val animationValues: List<Float> = (0 until ballCount).map { index ->
-        var animatedValue by remember { mutableStateOf(0f) }
+    val alphas: List<Float> = (0 until ballCount).map { index ->
+        var animatedValue by remember { mutableStateOf(maxAlpha) }
 
         LaunchedEffect(key1 = Unit) {
 
             if (index == ballCount / 2)
-                delay(200)
+                delay(animationDelay)
 
             animate(
-                initialValue = 1f,
-                targetValue = 0.7f,
+                initialValue = maxAlpha,
+                targetValue = minAlpha,
                 animationSpec = infiniteRepeatable(
-                    animation = tween(durationMillis = 350),
+                    animation = tween(durationMillis = animationDuration),
                     repeatMode = RepeatMode.Reverse,
                 ),
             ) { value, _ ->
@@ -48,19 +52,27 @@ fun BallBeatIndicator(
     }
 
 
-    Row {
+    Canvas(modifier = Modifier) {
+
+        val center = Offset(0f, 0f)
+        val xOffset = ballDiameter + spaceBetweenBalls
+        val yOffset = center.y
+
         for (index in 0 until ballCount) {
-            Canvas(
-                modifier = Modifier
-                    .padding(horizontal = 10.dp)
-            ) {
-                drawCircle(
-                    color = color,
-                    radius = 30f * animationValues[index],
-                    alpha = animationValues[index]
+
+            drawCircle(
+                color = color,
+                radius = (ballDiameter / 2) * alphas[index],
+                alpha = alphas[index],
+                center = Offset(
+                    x = when {
+                        index < ballCount / 2 -> -(center.x + xOffset)
+                        index == ballCount / 2 -> center.x
+                        else -> center.x + xOffset
+                    },
+                    y = yOffset
                 )
-            }
+            )
         }
     }
-
 }
