263991523c28cb89167c5e77315b4de3ba692359
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2023-09-16 22:13:27 +0330

    refactor: refactor BallPulseSyncIndicator

--- app/src/main/java/com/example/jetpackloading/ui/theme/loading_component/BallPulseSyncIndicator.kt
+++ app/src/main/java/com/example/jetpackloading/ui/theme/loading_component/BallPulseSyncIndicator.kt
@@ -5,41 +5,39 @@ import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.example.jetpackloading.ANIMATION_DEFAULT_COLOR
 import kotlinx.coroutines.delay
 
 @Composable
 fun BallPulseSyncIndicator(
     color: Color = ANIMATION_DEFAULT_COLOR,
-    ballCount: Int = 3,
-    delay: Long = 90L
+    delay: Long = 90L,
+    spaceBetweenBalls: Float = 20f,
+    ballDiameter: Float = 40f,
+    animationDuration: Int = 350
 ) {
 
-    val animationValues = (1..ballCount).map { index ->
+    val ballCount = 3
+
+    val positions = (1..ballCount).map { index ->
 
         var animatedValue: Float by remember { mutableStateOf(0f) }
 
         LaunchedEffect(key1 = Unit) {
-            delay(delay * index)
+            delay(delay + delay * index)
 
             animate(
-                initialValue = 0f,
-                targetValue = 18f,
-                animationSpec = infiniteRepeatable(
-                    animation = tween(durationMillis = 350),
+                initialValue = 0f, targetValue = 50f, animationSpec = infiniteRepeatable(
+                    animation = tween(durationMillis = animationDuration),
                     repeatMode = RepeatMode.Reverse,
                 )
             ) { value, _ -> animatedValue = value }
@@ -48,19 +46,22 @@ fun BallPulseSyncIndicator(
         animatedValue
     }
 
-    Row(modifier = Modifier, verticalAlignment = Alignment.CenterVertically) {
-        for (animatedValue in animationValues) {
-            Canvas(
-                modifier = Modifier
-                    .padding(horizontal = 10.dp)
-                    .offset(y = animatedValue.dp)
-                    .padding(bottom = 48.dp),
-            ) {
-                drawCircle(
-                    color = color,
-                    radius = 30f
+    Canvas(modifier = Modifier) {
+
+        val center = Offset(0f, 0f)
+        val xOffset = ballDiameter + spaceBetweenBalls
+
+        for (index in 0 until ballCount) {
+            drawCircle(
+                color = color, radius = ballDiameter / 2, center = Offset(
+                    x = when {
+                        index < ballCount / 2 -> -(center.x + xOffset)
+                        index == ballCount / 2 -> center.x
+                        else -> center.x + xOffset
+                    },
+                    y = center.y - positions[index]
                 )
-            }
+            )
         }
     }
 }
